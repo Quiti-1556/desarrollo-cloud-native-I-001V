6e368421a22d197ffaa9a62651be34d9cc42ae5f
Quiti-1556 <br.quitian@duocuc.cl>
2026-08-31 20:03:38 -0400

    first commit

---
+++ main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Crear la API HTTP
+resource "aws_apigatewayv2_api" "api" {
+  name          = "tutorial-api"
+  protocol_type = "HTTP"
+}
+
+# Crear la etapa (Stage) de despliegue automático
+resource "aws_apigatewayv2_stage" "default" {
+  api_id      = aws_apigatewayv2_api.api.id
+  name        = "$default"
+  auto_deploy = true
+}
+
+# Mostrar la URL final en la consola
+output "api_url" {
+  value       = aws_apigatewayv2_api.api.api_endpoint
+  description = "La URL pública de tu API Gateway"
+}
